3b86f32795b9979fb447afe9daccfd238128bc97
SajariS <santeri.sajari@gmail.com>
2023-11-09 16:44:14 +0200

    CustomerList korjaus

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import CustomerList from "./components/CustomerList"
 
+import Toolbar from "@mui/material/Toolbar"
 import Container  from "@mui/material/Container"
+import Typography from "@mui/material/Typography"
+import AppBar from "@mui/material/AppBar";
 
 function App() {
 
   return (
-    <Container maxWidth="xl">
+    <Container maxWidth="lg">
+      <AppBar position='static'>
+        <Toolbar>
+          <Typography variant="h6">Customers</Typography>
+        </Toolbar>
+      </AppBar>
       <CustomerList />
     </Container>
   )
